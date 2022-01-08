2978fef6b0baa7b5ad15b85fd57491eb309f8801
Ankit Gupta <anky.gupta9999@gmail.com>
2022-01-08 21:51:18 +0530

    makeit better

--- server.sh
+++ server.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# cd app/
-# python -m spacy download en_core_web_sm
-
-if [ -z "$PORT"]
-then
-  PORT=5005
-fi
-
 # Start rasa server with nlu model
-rasa run --enable-api --cors "*" -p 5005 --debug
+rasa run --enable-api --cors "*"  --debug
